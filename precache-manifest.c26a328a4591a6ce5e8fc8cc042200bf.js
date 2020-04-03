5be64d677c01564001789bc58e408fd7932ca40e
Akshatha Thonnuru Swamygowd <akshatha0505@gmail.com>
2020-04-03 15:24:46 -0700

    Updates

---
+++ precache-manifest.c26a328a4591a6ce5e8fc8cc042200bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3389b5c7eacb7cc74d99ede24fcfa24c",
+    "url": "/React-Movie/index.html"
+  },
+  {
+    "revision": "97dfdeba09a6fee829ce",
+    "url": "/React-Movie/static/css/main.1bdf874d.chunk.css"
+  },
+  {
+    "revision": "c66d285571437c37f7f1",
+    "url": "/React-Movie/static/js/2.f8099486.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Movie/static/js/2.f8099486.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97dfdeba09a6fee829ce",
+    "url": "/React-Movie/static/js/main.70b09ed6.chunk.js"
+  },
+  {
+    "revision": "333d562e3faa56a5a7e1",
+    "url": "/React-Movie/static/js/runtime-main.50eadea2.js"
+  }
+]);
